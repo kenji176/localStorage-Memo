d0f8bd7338aa695a9ddb5787047f42bde5bfb181
sinc <73232388+kenji176@users.noreply.github.com>
2022-05-21 21:44:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ function escapeHtml(str) {
 }
 
 function generateUuid() {
-	const chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
-	for (const i = 0, len = chars.length; i < len; i++) {
+	let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+	for (let  i = 0, len = chars.length; i < len; i++) {
 		switch (chars[i]) {
 			case "x":
 				chars[i] = Math.floor(Math.random() * 16).toString(16);
