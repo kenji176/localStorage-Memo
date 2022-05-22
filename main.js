968fbde4d7a3c471ce8b1a977661677c7dbcb890
sinc <73232388+kenji176@users.noreply.github.com>
2022-05-22 19:25:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,34 @@ window.onload = function() {
 				`  <div class="card mt-2 bg-dark">
                                                     <div class="card-body">
                                                         <div class="row">
-                                                            <div class="col">
-                                                                <div class="text-start text-white font-monospace">${(localStorage.getItem(localStorage.key(i)))}</div>
+                                                            <div class="col-7">
+                                                                <div class="text-start text-white font-monospace text-wrap">${(localStorage.getItem(localStorage.key(i)))}</div>
                                                             </div>
-                                                            <div class='col'>
+                                                            <div class='col-5'>
                                                                 <div class="text-end">
-                                                                    <input class="btn btn-sm btn-danger" type="button"  id=${localStorage.key(i)} value="削除"  onclick="Remove_(this);">
+																<div class="btn-group" role="group" aria-label="Basic example">
+																	<input class="btn btn-sm btn-danger" type="button"  id=${localStorage.key(i)} value="削除"  onclick="Remove_(this);">
+																	<button type="button" class="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#modal-${localStorage.key(i)}">編集</button>
+																	</div>
+																	<div class="modal fade" id="modal-${localStorage.key(i)}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+																		<div class="modal-dialog">
+																			<div class="modal-content">
+																			<div class="modal-header">
+																				<h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+																				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+																			</div>
+																			<div class="modal-body text-start">
+																				<div class="mb-3">
+																					<label for="exampleFormControlTextarea1" class="form-label"></label>
+																					<textarea class="form-control" id="sender-${localStorage.key(i)}"  rows="4" >${(localStorage.getItem(localStorage.key(i)))}</textarea>
+																				</div>
+																			</div>
+																			<div class="modal-footer">
+																				<button type="button" class="btn btn-primary"  data-bs-dismiss="modal" id="${localStorage.key(i)}" onclick="Update_(this);" >Save</button>
+																			</div>
+																			</div>
+																		</div>
+																		</div>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -22,14 +44,23 @@ window.onload = function() {
         }
 	}
 
+function Update_(element) {
+	const U_text = document.getElementById("sender-"+element.id).value
+	if (U_text == "") {
+		alert('文字数が足りません');
+	} else {
+		window.localStorage.setItem(element.id,U_text);
+		location.reload();
+	}
+}
+
 function OnButtonClick() {
 	const content = document.getElementById('send').value
 	if (content == "") {
 		alert('文字数が足りません');
 	} else {
-		const escapetext = escapeHtml(content);
-		console.log(escapetext);
-		window.localStorage.setItem(generateUuid(),escapetext);
+		localStorage.removeItem(this.id);
+		window.localStorage.setItem(generateUuid(),escapeHtml(content));
 		location.reload();
 	}
 }
@@ -72,4 +103,3 @@ function Remove_(element) {
         }
     }
 }
-
